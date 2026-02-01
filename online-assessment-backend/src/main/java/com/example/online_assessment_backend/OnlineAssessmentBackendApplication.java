bd00618210893c1548b6c9731dbf0b4e10c955dc
Vishal <vishal.200087@gmail.com>
2026-02-01 22:39:55 +0530

    Online Assessment System

---
+++ online-assessment-backend/src/main/java/com/example/online_assessment_backend/OnlineAssessmentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.online_assessment_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineAssessmentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineAssessmentBackendApplication.class, args);
+	}
+
+}
---
+++ online-assessment-backend/src/test/java/com/example/online_assessment_backend/OnlineAssessmentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_assessment_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineAssessmentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
