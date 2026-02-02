09fe1c1adc7ba15579e8e29948663baa5f5101eb
Vishal <vishal.200087@gmail.com>
2026-02-02 21:38:42 +0530

    Development Started

---
+++ online-assessment-backend/src/main/java/com/example/online_assessment_backend/entity/ExamEntity.java
@@ -0,0 +1,40 @@
+package com.example.online_assessment_backend.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "exams")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ExamEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+private String title;
+
+
+
+    private int durationMinutes;
+
+    @Column(nullable = false)
+private Boolean active;
+
+}
+
