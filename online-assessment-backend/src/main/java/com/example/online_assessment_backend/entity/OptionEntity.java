4f7962c412475157dc15f1ce611cd31c4ff45c1a
Vishal <vishal.200087@gmail.com>
2026-02-03 17:37:12 +0530

    Development started

---
+++ online-assessment-backend/src/main/java/com/example/online_assessment_backend/entity/OptionEntity.java
@@ -0,0 +1,36 @@
+package com.example.online_assessment_backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "options")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OptionEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id", nullable = false)
+    private QuestionEntity question;
+
+    private String optionText;
+
+    private boolean correct;
+}
