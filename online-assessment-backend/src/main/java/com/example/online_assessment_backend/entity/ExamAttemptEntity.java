2be79609a881dcec0c2d581e218cbc2cdcfafcce
Vishal <vishal.200087@gmail.com>
2026-02-04 21:06:40 +0530

    Development Going on

---
+++ online-assessment-backend/src/main/java/com/example/online_assessment_backend/entity/ExamAttemptEntity.java
@@ -0,0 +1,51 @@
+package com.example.online_assessment_backend.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "exam_attempts")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ExamAttemptEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "exam_id")
+    private ExamEntity exam;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "student_id")
+    private UserEntity student;
+
+    @Column(name = "start_time", nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time")
+    private LocalDateTime endTime;
+
+    @PrePersist
+    public void prePersist() {
+        this.startTime = LocalDateTime.now();
+    }
+}
