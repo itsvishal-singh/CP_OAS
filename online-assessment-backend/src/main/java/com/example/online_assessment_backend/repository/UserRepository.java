4a2e9012ba5fa93686bba21aa0144975bb14acf8
Vishal <vishal.200087@gmail.com>
2026-02-07 21:15:52 +0530

    Development Going on

---
+++ online-assessment-backend/src/main/java/com/example/online_assessment_backend/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.online_assessment_backend.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.online_assessment_backend.entity.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByEmail(String email);
+}
