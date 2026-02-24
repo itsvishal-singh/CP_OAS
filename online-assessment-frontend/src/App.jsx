612f62fc314dc58cf642a07a334a0ef12ed7140d
Vishal <vishal.200087@gmail.com>
2026-02-24 14:01:23 +0530

    FrontEnd Setup Successfull

---
+++ online-assessment-frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center text-3xl font-bold text-blue-600">
+      Online Assessment System 🚀
+    </div>
+  );
+}
+
+export default App;
