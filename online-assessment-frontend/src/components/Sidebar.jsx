f6ef8de375302fbaa473f10442b32db6ddfa2fec
Vishal <vishal.200087@gmail.com>
2026-03-13 16:19:56 +0530

    Tried to arrange Buttons in correct flow

--- online-assessment-frontend/src/components/Sidebar.jsx
+++ online-assessment-frontend/src/components/Sidebar.jsx
@@ -23,7 +23,7 @@ export default function Sidebar() {
           to="/admin/students"
           className="px-4 py-2 rounded hover:bg-indigo-700 transition"
         >
-          Students
+          Manage Users
         </Link>
         <Link
           to="/admin/exams"
@@ -36,7 +36,7 @@ export default function Sidebar() {
           to="/admin/questions"
           className="px-4 py-2 rounded hover:bg-indigo-700 transition"
         >
-          Questions
+          Manage Questions
         </Link>
 
         <Link
