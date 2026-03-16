e2b60d6281e0fc0351437cef89c6d133f39703c7
Vishal <vishal.200087@gmail.com>
2026-03-16 21:27:03 +0530

    Exam Attempt Entity and Repository

--- online-assessment-backend/src/main/java/com/example/online_assessment_backend/entity/ExamAttemptEntity.java
+++ online-assessment-backend/src/main/java/com/example/online_assessment_backend/entity/ExamAttemptEntity.java
@@ -11,6 +11,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,7 +19,11 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "exam_attempts")
+@Table(
+    uniqueConstraints = @UniqueConstraint(
+        columnNames = {"exam_id", "student_id", "status"}
+    )
+)
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
--- online-assessment-backend/src/main/java/com/example/online_assessment_backend/repository/ExamAttemptRepository.java
+++ online-assessment-backend/src/main/java/com/example/online_assessment_backend/repository/ExamAttemptRepository.java
@@ -1,5 +1,6 @@
 package com.example.online_assessment_backend.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,7 +10,7 @@ import com.example.online_assessment_backend.entity.ExamAttemptEntity;
 public interface ExamAttemptRepository
                 extends JpaRepository<ExamAttemptEntity, Long> {
 
-        Optional<ExamAttemptEntity> findByExam_IdAndStudent_IdAndStatus(
+        List<ExamAttemptEntity> findByExam_IdAndStudent_IdAndStatus(
                         Long examId,
                         Long studentId,
                         String status);
