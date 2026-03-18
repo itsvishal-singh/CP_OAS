3399d2c85bf087f87eb78feea0138043b45a7e2a
Vishal <vishal.200087@gmail.com>
2026-03-18 22:12:41 +0530

    API Added

--- online-assessment-backend/src/main/java/com/example/online_assessment_backend/controller/StudentDashboardController.java
+++ online-assessment-backend/src/main/java/com/example/online_assessment_backend/controller/StudentDashboardController.java
@@ -3,10 +3,14 @@ package com.example.online_assessment_backend.controller;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.online_assessment_backend.dto.StudentDashboardResponse;
+import com.example.online_assessment_backend.entity.UserEntity;
+import com.example.online_assessment_backend.repository.UserRepository;
 import com.example.online_assessment_backend.service.ExamService;
 
 import lombok.RequiredArgsConstructor;
@@ -17,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 public class StudentDashboardController {
 
   private final ExamService examService;
+  private UserRepository userRepository;
 
   @GetMapping("/dashboard")
   public StudentDashboardResponse dashboard(
@@ -24,5 +29,23 @@ public class StudentDashboardController {
 
     return examService
         .getStudentDashboard(auth.getName());
+  }
+
+  @GetMapping("/profile")
+  public UserEntity getProfile(Authentication auth) {
+    return userRepository.findByUsername(auth.getName())
+        .orElseThrow(() -> new RuntimeException("User not found"));
+  }
+
+  @PutMapping("/profile")
+  public UserEntity updateProfile(@RequestBody UserEntity updatedUser, Authentication auth) {
+
+    UserEntity user = userRepository.findByUsername(auth.getName())
+        .orElseThrow(() -> new RuntimeException("User not found"));
+
+    user.setFullName(updatedUser.getFullName());
+    user.setMobile(updatedUser.getMobile());
+
+    return userRepository.save(user);
   }
 }
