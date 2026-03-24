9b6bb3704c9b28d48ad7b6011a1ff085d9f27984
Vishal <vishal.200087@gmail.com>
2026-03-24 22:07:44 +0530

    UI changed

--- online-assessment-frontend/src/pages/StudentDashboard.jsx
+++ online-assessment-frontend/src/pages/StudentDashboard.jsx
@@ -48,7 +48,7 @@ export default function StudentDashboard() {
   if (!stats) return <p className="text-white">Loading...</p>;
 
   return (
-    <div className="p-8 bg-gradient-to-r from-indigo-700 via-blue-500 to-purple-700 h-[calc(100vh-99px)]">
+    <div className="p-8 bg-gradient-to-b from-indigo-700 via-blue-500 to-purple-700 h-[calc(100vh-99px)]">
       <h1 className="text-5xl text-white font-semibold m-8 pb-8 text-center ">
         Your Home
       </h1>
