d63474ac9adb217cbf75ef3e70aa1370e7a63b72
Vishal <vishal.200087@gmail.com>
2026-03-24 22:07:56 +0530

    UI improved

--- online-assessment-frontend/src/pages/StudentExams.jsx
+++ online-assessment-frontend/src/pages/StudentExams.jsx
@@ -25,42 +25,46 @@ export default function StudentExams() {
   };
 
   return (
-    <div className="p-10 bg-gray-50 min-h-screen">
-  
-      <h1 className="text-3xl font-bold mb-8 text-indigo-700">
+    <div className="p-12 bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-purple-600 via-blue-600 to-indigo-700 min-h-screen">
+      <h1 className="text-4xl font-bold mb-8 text-white text-center">
         🚀 Available Exams
       </h1>
-  
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-6">
         {exams.map((exam) => (
           <div
             key={exam.id}
-            className="bg-white rounded-2xl shadow-md p-6 hover:shadow-xl transition duration-300 flex flex-col justify-between"
+            className="bg-white rounded-2xl shadow-md p-6 hover:scale-[1.02] transition-transform duration-500 flex flex-col justify-between text-center"
           >
             <div>
-              <h2 className="text-xl font-bold text-gray-800 mb-2">
+              <h2 className="text-xl font-bold text-blue-600 mb-2">
                 {exam.title}
               </h2>
-  
-              <p className="text-gray-500 mb-1">
-                ⏱ Duration: <span className="font-semibold">{exam.durationMinutes} min</span>
+
+              <p className="text-blue-500 mb-1">
+                ⏱ Duration:{" "}
+                <span className="font-bold text-blue-700">
+                  {exam.durationMinutes} min
+                </span>
               </p>
-  
-              <p className="text-gray-500">
-                🎯 Marks: <span className="font-semibold">{exam.totalMarks}</span>
+
+              <p className="text-blue-500">
+                🎯 Marks:{" "}
+                <span className="font-bold text-blue-700">
+                  {exam.totalMarks}
+                </span>
               </p>
             </div>
-  
+
             <button
               onClick={() => startExam(exam.id)}
-              className="mt-5 bg-gradient-to-r from-indigo-500 to-purple-600 text-white py-2 rounded-lg hover:scale-105 transition"
+              className="mt-4 bg-gradient-to-r from-blue-700 via-indigo-700 to-purple-700 text-white font-semibold py-2 mx-8 rounded-xl hover:scale-105 transition"
             >
               Start Exam →
             </button>
           </div>
         ))}
       </div>
-  
     </div>
   );
 }
